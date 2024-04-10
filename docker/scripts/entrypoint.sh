632eda4456d845154e28482103eee3bb867d736c
Leandro Ramos <leandroramos@disroot.org>
2024-04-10 16:20:04 -0300

    Add airflow db init command to entrypoint

--- docker/scripts/entrypoint.sh
+++ docker/scripts/entrypoint.sh
@@ -190,6 +190,8 @@ function set_pythonpath_for_root_user() {
     fi
 }
 
+airflow db init
+
 function wait_for_airflow_db() {
     # Wait for the command to run successfully to validate the database connection.
     run_check_with_retries "airflow db check"
